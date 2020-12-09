67263c0a535641edcce49680d3e0f2369726d35c
Salman Zafar <salman.zafar@innowi.com>
2020-12-09 13:17:31 +0500

    code refactoring

--- index.php
+++ index.php
@@ -6,15 +6,4 @@ $greeting = "Hello World";
 
 $todos = $query->all('todos');
 
-/*$todos  = [
-    new Todo('Learn Core Php'),
-    new Todo('Learn Laravel', true),
-    new Todo('Learn Go'),
-    new Todo('Learn Python'),
-    new Todo('Learn Django'),
-    new Todo('Learn React Js', true),
-    new Todo('Learn Vue Js', true),
-];*/
-//dd($greeting);
-
 require "index.view.php";
